5d109a41da84625470ee141b3da78198a4d0f6cd
Milton-668 <miltonmjcp@gmail.com>
2021-02-24 19:24:52 -0300

    Produto

--- mygamestore/src/main/java/com/mygamestore/mygamestore/Model/Categoria.java
+++ mygamestore/src/main/java/com/mygamestore/mygamestore/Model/Categoria.java
@@ -1,11 +1,17 @@
 package com.mygamestore.mygamestore.Model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -17,6 +23,10 @@ public class Categoria {
 
 	@NotNull
 	private String tipo;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties ("categoria")
+	private List<Produto> postagem;
 
 	public long getId() {
 		return id;
@@ -33,5 +43,14 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	public List<Produto> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Produto> postagem) {
+		this.postagem = postagem;
+	}
+	
 
 }
---
+++ mygamestore/src/main/java/com/mygamestore/mygamestore/Model/Produto.java
@@ -0,0 +1,54 @@
+package com.mygamestore.mygamestore.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Primary Key
+	private long id;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String produto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
